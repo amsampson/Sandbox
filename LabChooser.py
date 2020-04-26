3b87107fe6c2d44eb1981b2cb0ff42fae2efd2d4
amsampson <64328628+amsampson@users.noreply.github.com>
2020-04-25 20:20:36 -0400

    Update to LabChooser.py
    to simplify code and add attendance

--- LabChooser.py
+++ LabChooser.py
@@ -1,53 +1,45 @@
-print('Hello, Thank you for choosing The Lab Chooser')
+print('Hello, Thank you for choosing The Lab Chooser 2.0')
 print('Password?')
 Password = input()
 while Password != 'Wowie Zowie':
     print ('Access Denied')
     print('Password?')
     Password = input()
-    
+
+print('Access granted')
+
 print()
 
 import random
-list = []
+list=[]
 
-def labchooser(n):
-    if n == 1 :
-        return 'Dan'
-    elif n == 2:
-        return 'Lynn'
-    elif n == 3:
-        return 'Lyndsey'
-    elif n == 4:
-        return 'Katie'
-    elif n == 5:
-        return 'Laura'
-    elif n == 6:
-        return 'Emily S'
-    elif n == 7:
-        return 'Avery'
-    elif n == 8:
-        return 'Tim'
-    elif n == 9:
-        return 'Josh'
-    elif n == 10:
-        return 'Chiharu'
-    elif n == 11:
-        return 'Emily C'
-    elif n == 12:
-        return 'Maddie'
-    elif n == 13:
-        return 'Puneet'
-    elif n == 14:
-        return 'LaQuita'
-    elif n == 15:
-        return 'Ashley'
-while len(list) < 15:
-    r = labchooser(random.randint(1,15))
-    if r not in list: list.append(r)
+### Attendence entry
+print('Please enter those in attendance')
+x = input()
+list.append(x)
+print('Please enter more. If finished type: Done')
+while x != 'Done': ### loop to type everybody in
+    x = input()
+    if x == 'Done':
+        break ### This keeps "Done" from being listed
+    else:
+        list.append(x)
+    print('Please enter more. If finished type: Done')
+
+random.shuffle(list)
 
 
-print(list)
 print()
-print('End Of Program, Thank You. Press ENTER to exit.')
+print('The order for today is...')
+
+### Order lister by line (will list vertically)
+r=0
+while r <= len(list):
+    if r == len(list):
+        break
+    print(list[r])
+    r = r+1
+    
+print()
+print('To exit: Please hit ENTER')
 input()
